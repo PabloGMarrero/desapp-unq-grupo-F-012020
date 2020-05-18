5a6813430d129f63f0d27b32ae6c979712e00649
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-05-18 17:17:33 -0300

    added documentation to geographicmap, openhours and turn classes

--- src/main/kotlin/unq/tpi/desapp/model/OpenHours.kt
+++ src/main/kotlin/unq/tpi/desapp/model/OpenHours.kt
@@ -4,6 +4,9 @@ import org.joda.time.DateTime
 
 /**
  * Open hours represents the concept which day is open with specific hour
+ * @param day represent the day when the store is open
+ * @param hourFrom represents from what hour is the store open
+ * @param hourTo represents to what hour is the store open
  */
 class OpenHours(day:String, hourFrom:DateTime, hourTo:DateTime){
     var day: String = day
--- src/main/kotlin/unq/tpi/desapp/model/Turn.kt
+++ src/main/kotlin/unq/tpi/desapp/model/Turn.kt
@@ -4,7 +4,9 @@ import java.time.LocalDate
 import java.time.LocalTime
 
 /**
- *
+ * Turn represents the idea of what day and at what time a turn is taken by a purchase
+ * @param day represents the day of the turn
+ * @param hour represents the hour of the turn
  */
 class Turn(day: LocalDate, hour: LocalTime) {
     var day = day
