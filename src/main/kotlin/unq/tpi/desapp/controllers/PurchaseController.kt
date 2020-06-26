00bed17f14b1301b1d251ff0acd1f40e2d0838a1
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-06-26 19:30:49 -0300

    add basic endpoint to manage purchases, still service needs little tweaks and validation

--- src/main/kotlin/unq/tpi/desapp/controllers/PurchaseController.kt
+++ src/main/kotlin/unq/tpi/desapp/controllers/PurchaseController.kt
@@ -1,4 +1,31 @@
 package unq.tpi.desapp.controllers
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import unq.tpi.desapp.aspects.LoggingAspect
+import unq.tpi.desapp.dto.PurchaseDto
+import unq.tpi.desapp.model.Purchase
+import unq.tpi.desapp.service.PurchaseService
+
+@RestController
+@RequestMapping("/purchase")
+@CrossOrigin(origins = ["*"])
 class PurchaseController {
+
+    @Autowired
+    var purchaseService: PurchaseService = PurchaseService()
+
+    @LoggingAspect
+    @PostMapping("/new", consumes = arrayOf("application/json"), produces = arrayOf("application/json"))
+    fun newPurchase(@RequestBody purchaseDto: PurchaseDto):ResponseEntity<Purchase>{
+        try{
+            var purchaseSaved = this.purchaseService.newPurchase(purchaseDto)
+            return ResponseEntity.ok().body(purchaseSaved)
+        }
+        catch (ex:Exception){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null)
+        }
+    }
 }
--- src/main/kotlin/unq/tpi/desapp/service/PurchaseService.kt
+++ src/main/kotlin/unq/tpi/desapp/service/PurchaseService.kt
@@ -1,4 +1,44 @@
 package unq.tpi.desapp.service
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import unq.tpi.desapp.builders.ItemBuilder
+import unq.tpi.desapp.builders.PurchaseBuilder
+import unq.tpi.desapp.builders.StoreBuilder
+import unq.tpi.desapp.dto.PurchaseDto
+import unq.tpi.desapp.model.*
+import unq.tpi.desapp.model.deliveryType.DeliveryType
+import unq.tpi.desapp.repository.ProductRepository
+import unq.tpi.desapp.repository.UserRepository
+import java.util.*
+
+@Service
+@Transactional
 class PurchaseService {
+
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @Autowired
+    lateinit var productRepository: ProductRepository
+
+    fun newPurchase(purchaseDto: PurchaseDto):Purchase{
+        var anUser:Optional<User> = userRepository.findById(purchaseDto.user.id)
+        var deliveryType: DeliveryType = purchaseDto.deliveryType
+        var paymentMethod: PaymentMethod = purchaseDto.paymentMethod
+
+        var purchase:Purchase = PurchaseBuilder.aPurchase().withUser(anUser.get()).withDeliveryType(deliveryType).withPaymentMethod(paymentMethod).build()
+        addProductsToPurchase(purchase, purchaseDto.products)
+        return purchase
+    }
+
+    private fun addProductsToPurchase(purchase: Purchase, products: MutableList<Product>){
+        for(product in products){
+            var store: Store = StoreBuilder.aStore().build()
+            var item: Item = ItemBuilder.anItem().withAStore(store).withProduct(product).withQuantity(1.0).build()
+            purchase.addItem(item)
+        }
+
+    }
 }
