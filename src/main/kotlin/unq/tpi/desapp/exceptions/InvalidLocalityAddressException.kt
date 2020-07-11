34ba50be40c2e4f7505d540fa64336a4b4e85f18
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-07-11 13:21:48 -0300

    added documentation to the exceptions

--- src/main/kotlin/unq/tpi/desapp/exceptions/InvalidLocalityAddressException.kt
+++ src/main/kotlin/unq/tpi/desapp/exceptions/InvalidLocalityAddressException.kt
@@ -2,5 +2,8 @@ package unq.tpi.desapp.exceptions
 
 import java.lang.RuntimeException
 
+/**
+ * Exception to handle the invalid address locality when the address is created
+ */
 class InvalidLocalityAddressException(message:String) : RuntimeException(message)  {
 }
